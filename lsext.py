9dfce0742e084993adf0c0a4c6c6df02aeaa452d
NoviceLive <rectigu@gmail.com>
2015-05-01 08:52:06 +0800

    no more lone lines and readme is not changelog

--- lsext.py
+++ lsext.py
@@ -1,66 +1,10 @@
 #!/usr/bin/env python3
 
+
 """
 file format distribution analyzer
 
 by Novice Live, http://novicelive.org :)
-
-23 mar 2015
-
-1. removed the function, `remove_items', thus making it more unpythonic.
-
-   buggy!
-   undebuggable!
-   illegible!
-
-   just for fun or challenge.  ^_^
-
-   consider pythonizing it next time.
-
-mar 11, 2015
-
-1. added `-i' option, used to ignore the specified directory name, e.g. .git.
-
-2. `argparse' is too verbose! consider wrapping it into a function.
-   or, wrapping the whole module to make a concise one.
-
-3. what a bad programmer!
-   what a mess!
-   `operate_all_files'.
-
-4. from now on, use git for version controlling,
-   not merely for transferring data between github and my local computer.
-
-feb 21, 2015
-
-1. employed str.lower since `.pdf' and `.PDF' should be the regarded as the same extension.
-   and assumed that all extensions' cases should be insignificant.
-
-2. changed function names: `compose_function' to `fcompose', `map_function' to `fmap'.
-
-feb 20, 2015
-
-1. completed the refactoring in the core function, `get_all_info'.
-
-feb 19, 2015
-
-1. changed to returning chained generators, instead of a list reduced from a list of lists;
-   in the the core function `get_all_info'.
-
-2. please refactor the core function `get_all_info', such that,
-   you can just collect the information specified by the extracting function which is provided as one of the arguments to `get_all_info'.
-   but now i am too exhausted. i have to sleep. let's fix it tomorrow.
-
-3. happy chinese new year! am i a hard-working and responsible employee, one who still keeps coding to deep night even in the new year's day.
-
-feb 14, 2015
-
-1. rewritten from scratch, using os.walk instead of using recursive os.listdir.
-
-2. added size and number distribution analysis.
-
-3. likely to add the feature of recognizing files based upon their magic numbers rather than the suffixes.
-   obviously, this feature will cost more time.
 
 Copyright (C) 2015  Gu Zhengxiong
 
@@ -78,40 +22,33 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
 import os
 import sys
 import itertools
 import operator
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(
-        description='analyze the distribution of different file formats in specified directories',
+        description="""
+analyze the distribution of different file formats in specified directories
+""",
         conflict_handler='resolve')
 
-    parser.add_argument('dirs',
-                        metavar='dir',
-                        nargs='*',
+    parser.add_argument('dirs', metavar='dir', nargs='*',
                         help='the directory to analyze')
-    parser.add_argument('-s', '--size',
-                        dest='size',
-                        action='store_true',
+    parser.add_argument('-s', '--size', dest='size', action='store_true',
                         help='analyze size distribution')
-    parser.add_argument('-n', '--number',
-                        dest='number',
-                        action='store_true',
+    parser.add_argument('-n', '--number', dest='number', action='store_true',
                         help='analyze number distribution')
-    parser.add_argument('-h', '--human',
-                        dest='human',
-                        choices=('k', 'm', 'g'),
+    parser.add_argument('-h', '--human', dest='human', choices=('k', 'm', 'g'),
                         default='b',
                         help='human-readable size in the specified unit')
-    parser.add_argument('-f', '--follow',
-                        dest='follow',
-                        action='store_true',
+    parser.add_argument('-f', '--follow', dest='follow', action='store_true',
                         help='follow symbolic links')
-    parser.add_argument('-i', '--ignore',
-                        dest='ignore',
+    parser.add_argument('-i', '--ignore', dest='ignore',
                         nargs='+',
                         help='ignore the specified directory names')
 
@@ -125,29 +62,55 @@ def main():
             print("{}: ".format(i))
 
             all_info = operate_all_files(i,
-                                         get_file_ext if not args.size else get_file_ext_size,
+
+                                         get_file_ext
+                                         if not args.size else
+                                         get_file_ext_size,
+
                                          args.follow,
                                          args.ignore if args.ignore else ()
             )
 
-            # need two independent generators if we still want to extract size information.
-            all_ext_info, all_ext_size_info = itertools.tee(all_info) if args.size else (all_info, None)
+            # need two independent generators
+            # if we still want to extract size information.
+
+            all_ext_info, all_ext_size_info = itertools.tee(
+                all_info
+            ) if args.size else (
+                all_info, None
+            )
 
             # treat extensions' cases insignificant.
-            all_exts = list(map(str.lower, all_ext_info)) if not args.size else list(map(fcompose(str.lower, operator.itemgetter(0)), all_ext_info))
+            all_exts = list(
+                map(str.lower, all_ext_info)
+            ) if not args.size else list(
+                map(fcompose(str.lower, operator.itemgetter(0)), all_ext_info)
+            )
 
             unique_exts = sorted(set(all_exts))
 
             if args.number:
-                num = sorted({i:all_exts.count(i) for i in unique_exts}.items(), key=operator.itemgetter(1), reverse=True)
+                num = sorted(
+                    {i:all_exts.count(i) for i in unique_exts}.items(),
+                    key=operator.itemgetter(1),
+                    reverse=True
+                )
+
                 stat_print(num, False)
 
             elif args.size:
                 size = {i:0 for i in unique_exts}
+
                 for i in all_ext_size_info:
                     # remember treating extensions' cases insignificant.
                     size[str.lower(i[0])] += i[1]
-                size = sorted(size.items(), key=operator.itemgetter(1), reverse=True)
+
+                size = sorted(
+                    size.items(),
+                    key=operator.itemgetter(1),
+                    reverse=True
+                )
+
                 stat_print(size, str_to_scale[args.human])
 
             else:
@@ -156,26 +119,35 @@ def main():
         else:
             print('could not access: {}\nmay not exist'.format(i))
 
+
 str_to_scale = {'b':1, 'k':1024, 'm':1024 * 1024, 'g':1024 * 1024 * 1024}
 
+
 scale_to_str = {1:'B', 1024:'KiB', 1024 * 1024:'MiB', 1024 * 1024 * 1024:'GiB'}
+
 
 # note using fcompose is surely slower than directly using f(g(x)).
 # but the direct form is not always applicable to all scenarios.
 fcompose = lambda f, g: lambda x: f(g(x))
 
+
 get_file_ext = fcompose(operator.itemgetter(1), os.path.splitext)
 
+
 get_file_size = os.path.getsize
+
 
 # fcombine or fmap?
 fmap = lambda f, g: lambda x: (f(x), g(x))
 
+
 get_file_ext_size = fmap(get_file_ext, get_file_size)
+
 
 def stat_print(source, scale):
     """
-    print all keys in the specified dictionary with consideration of the weight of its value.
+    print all keys in the specified dictionary
+    with consideration of the weight of its value.
     """
     total = sum(map(operator.itemgetter(1), source))
     [
@@ -189,6 +161,7 @@ def stat_print(source, scale):
     ]
     return
 
+
 # def remove_items(old, items):
 #     # use list comprehension to loop over a list, but discard the results.
 #     # did this waste anything?
@@ -196,16 +169,19 @@ def stat_print(source, scale):
 #     [old.remove(i) for i in items if i in old]
 #     return old
 
+
 def operate_all_files(directory, operation_func, follow, ignore_dirs):
     """
     perform the operation specified by `operation_func'
-    upon all files in the specified directory and its subdirectories, recursively,
+    upon all files in the specified directory and its subdirectories,
+    recursively,
     excluding those in directoies whose name are specified in `ignore_dirs'.
     """
     return itertools.chain(
         *(
             # introduce an independent namespace
-            # to avoid the undesired results caused by the undesired lazy evaluation of generator expressions.
+            # to avoid the undesired results caused by
+            # the undesired lazy evaluation of generator expressions.
             (
                 lambda x: (
                     operation_func(os.path.join(x[0], i))
@@ -213,11 +189,13 @@ def operate_all_files(directory, operation_func, follow, ignore_dirs):
                 )
             )
             (
-                # update the directory list which will be traversed later by os.walk
+                # update the directory list
+                # which will be traversed later by os.walk
                 # to let os.walk ignore the specified directory names
                 (lambda x: (
                     x[0],
-                    # removed the function `remove_items', thus making it more unpythonic
+                    # removed the function `remove_items',
+                    # thus making it more unpythonic
                     list(map(x[1].remove, set(ignore_dirs) & set(x[1]))),
                     x[2]))
                 (i)
@@ -227,8 +205,9 @@ def operate_all_files(directory, operation_func, follow, ignore_dirs):
         )
     )
 
+
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('warning: user cancelled')
+        print('[!] user cancelled')
